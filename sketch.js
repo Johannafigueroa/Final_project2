c6ff209e235d79426922239d3654839d879a5be7
HoangNguyen0524 <40507081+HoangNguyen0524@users.noreply.github.com>
2019-07-05 10:00:36 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,4 +2,17 @@ function setup()
 {  
    createCanvas(800, 400);
    background('black');
+   noStroke();
+}
+
+function draw()
+{
+    drawCircle();
+}
+
+function drawCircle()
+{
+    background('black');
+    ellipse(mouseX, mouseY, 25, 25);
+    
 }
